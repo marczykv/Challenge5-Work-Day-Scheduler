47b22eac2cf7bd5cbe222fb51340253422ead107
Victor Marczyk <marczykv@gmail.com>
2023-07-07 00:17:55 -0400

    reworked save feature

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,14 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-   // Function to save the user input in local storage
-   $(".saveBtn").on("click", function () {
+  // Function to save the user input in local storage
+  $(".saveBtn").on("click", function () {
     var description = $(this).siblings(".description").val().trim();
     var timeBlockId = $(this).parent().attr("id");
     localStorage.setItem(timeBlockId, description);
   });
 
-   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // Function to apply the past, present, or future class to each time block
   function applyTimeBlockStyles() {
-    var currentHour = dayjs().format("H");
+    var currentHour = dayjs().hour();
     $(".time-block").each(function () {
       var timeBlockId = $(this).attr("id");
       var hour = parseInt(timeBlockId.split("-")[1]);
@@ -29,7 +16,7 @@ $(function () {
       if (hour < currentHour) {
         $(this).addClass("past");
         $(this).removeClass("present future");
-      } else if (hour == currentHour) {
+      } else if (hour === currentHour) {
         $(this).addClass("present");
         $(this).removeClass("past future");
       } else {
@@ -50,41 +37,48 @@ $(function () {
     var startHour = 9;
     var endHour = 17;
 
-    $(".time-block").remove();
-
     for (var hour = startHour; hour <= endHour; hour++) {
       var timeBlockId = "hour-" + hour;
       var amPmHour = hour > 12 ? hour - 12 : hour;
       var amPm = hour >= 12 ? "PM" : "AM";
       var displayHour = amPmHour + amPm;
 
-      var timeBlock = $("<div>").attr("id", timeBlockId).addClass("row time-block");
-      var hourCol = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(displayHour);
-      var descriptionTextarea = $("<textarea>").addClass("col-8 col-md-10 description").attr("rows", "3");
-      var saveBtn = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save");
-      var saveIcon = $("<i>").addClass("fas fa-save").attr("aria-hidden", "true");
+      var timeBlock = $("<div>")
+        .attr("id", timeBlockId)
+        .addClass("row time-block");
+      var hourCol = $("<div>")
+        .addClass("col-2 col-md-1 hour text-center py-3")
+        .text(displayHour);
+      var descriptionTextarea = $("<textarea>")
+        .addClass("col-8 col-md-10 description")
+        .attr("rows", "3");
+      var saveBtn = $("<button>")
+        .addClass("btn saveBtn col-2 col-md-1")
+        .attr("aria-label", "save");
+      var saveIcon = $("<i>")
+        .addClass("fas fa-save")
+        .attr("aria-hidden", "true");
 
       saveBtn.append(saveIcon);
       timeBlock.append(hourCol, descriptionTextarea, saveBtn);
-      $(".container-fluid").append(timeBlock);
+      $("#schedulerContainer").append(timeBlock);
     }
   }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // Function to display the saved events from local storage
   function displaySavedEvents() {
     $(".time-block").each(function () {
       var timeBlockId = $(this).attr("id");
       var savedDescription = localStorage.getItem(timeBlockId);
-      $(this).find(".description").val(savedDescription);
+      if (savedDescription) {
+        $(this).find(".description").val(savedDescription);
+      }
     });
   }
 
   // Call the necessary functions to initialize the scheduler
-  applyTimeBlockStyles();
   displayCurrentDate();
   createTimeBlocks();
+  applyTimeBlockStyles();
   displaySavedEvents();
-  
 });
